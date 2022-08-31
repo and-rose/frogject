a05784578f8c8f235296cf9390670fa873689fa9
Braith Anderson <braithanderson1@gmail.com>
2022-08-31 20:14:38 +1000

    Added Roboto Font from expo Added React Navigation

--- App.tsx
+++ App.tsx
@@ -3,23 +3,53 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Provider as PaperProvider, Text } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 export default function App() {
-    return (
-        <PaperProvider>
-            <View style={styles.container}>
-                <Text style={{ color: "green" }} variant={"headlineLarge"}>
-                    Leaper
-                    <MaterialCommunityIcons
-                        name="debug-step-over"
-                        size={35}
-                        color="green"
-                    />
-                </Text>
-                <StatusBar style="auto" />
-            </View>
-        </PaperProvider>
-    );
+    let [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <ActivityIndicator />;
+    } else {
+        return (
+            <PaperProvider>
+                <View style={styles.container}>
+                    <StatusBar style="auto" />
+                    <View
+                        style={{
+                            display: "flex",
+                            flex: 1,
+                            justifyContent: "space-around",
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: MD2Colors.green700,
+                                textTransform: "uppercase",
+                                fontFamily: "Roboto_400Regular",
+                                letterSpacing: 1.5,
+                            }}
+                            variant={"headlineLarge"}
+                        >
+                            Leaper
+                            <MaterialCommunityIcons
+                                name="debug-step-over"
+                                size={35}
+                                color={MD2Colors.green700}
+                            />
+                        </Text>
+                        <ActivityIndicator
+                            color={MD2Colors.green700}
+                            size={40}
+                        />
+                    </View>
+                </View>
+            </PaperProvider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
