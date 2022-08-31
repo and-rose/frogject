971c1475fea66be902a2ca5e0ca4095d24ac384c
Braith Anderson <braithanderson1@gmail.com>
2022-08-31 17:49:09 +1000

    Added Leaper text to splash screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as PaperProvider, Text } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
     return (
         <PaperProvider>
             <View style={styles.container}>
-                <MaterialCommunityIcons
-                    name="debug-step-over"
-                    size={35}
-                    color="green"
-                />
+                <Text style={{ color: "green" }} variant={"headlineLarge"}>
+                    Leaper
+                    <MaterialCommunityIcons
+                        name="debug-step-over"
+                        size={35}
+                        color="green"
+                    />
+                </Text>
                 <StatusBar style="auto" />
             </View>
         </PaperProvider>
