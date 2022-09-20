a9f90f3b87bca63bc2481357a9eb86d88e9a5ab8
Braith Anderson <braithanderson1@gmail.com>
2022-09-20 13:06:46 +1000

    Tweaked percentage for smoother swiping

--- src/components/SeekProfileCard.tsx
+++ src/components/SeekProfileCard.tsx
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
     },
     card: {
         width: '100%',
-        height: '84%',
+        height: '83.5%',
         alignSelf: 'center',
         backgroundColor: 'white',
     },
